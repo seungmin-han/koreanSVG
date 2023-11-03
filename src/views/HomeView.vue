6f3279fcb0aca2c1ee9dd8791e86d5a64f06ad61
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-11-03 15:21:46 +0900

    Update: svg 컴포넌트 추가 및 라우팅 정리

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+	<div>
+		<Map></Map>
+	</div>
 </template>
+
+<script setup>
+import Map from '@/components/Map';
+</script>
